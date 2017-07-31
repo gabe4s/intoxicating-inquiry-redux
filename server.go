fa52c9e96071f4acda3dd45aecf9643be00af85b
Gabe Douda <gabedouda@gmail.com>
2017-07-30 20:51:26 -0600

    added creation of cookies

--- server.go
+++ server.go
@@ -4,37 +4,32 @@ import (
   "fmt"
   "log"
   "net/http"
-  "encoding/json"
+  "time"
 )
-
-type user struct {
-  name string
-}
 
 func main() {
   port := "8080";
 
-  http.HandleFunc("/api/test", Test)
-  http.HandleFunc("/login", Login)
   http.HandleFunc("/img/", serveResource)
   http.HandleFunc("/css/", serveResource)
+  http.HandleFunc("/", serveHtml)
+  http.HandleFunc("/webservice/login", loginService)
 
   fmt.Println("Server listening on port " + port);
   log.Fatal(http.ListenAndServe(":" + port, nil))
 }
 
-func Test(w http.ResponseWriter, r *http.Request) {
-  usera := user{name: "testname"}
-  w.Header().Set("Content-Type", "application/json")
-  j, _ := json.Marshal(usera)
-  w.Write(j)
+func serveHtml(w http.ResponseWriter, r *http.Request) {
+  http.ServeFile(w, r, "assets/html/" + r.URL.Path[1:] + ".html")
 }
 
-func Login(w http.ResponseWriter, r *http.Request) {
-  http.ServeFile(w, r, "assets/html/login.html")
+func serveResource(w http.ResponseWriter, r *http.Request) {
+  path := "assets" + r.URL.Path
+  http.ServeFile(w, r, path)
 }
 
-func serveResource(w http.ResponseWriter, req *http.Request) {
-  path := "assets" + req.URL.Path
-  http.ServeFile(w, req, path)
+func loginService(w http.ResponseWriter, r *http.Request) {
+  cookie := http.Cookie{Name: "intoxicating_inquiry_session_cookie", Value: "23fwfffw5tye", Expires: time.Now().Add(1 * 24 * time.Hour), Path: "/"}
+  http.SetCookie(w, &cookie)
+  http.Redirect(w, r, "/home", http.StatusFound)
 }
