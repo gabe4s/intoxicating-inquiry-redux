1c0a5d1a5a69e2e89fcda3288eb55209e410b127
Gabe Douda <gabedouda@gmail.com>
2017-09-02 15:30:52 -0600

    added basic session routing

--- server.go
+++ server.go
@@ -20,15 +20,42 @@ func main() {
   http.HandleFunc("/img/", serveResource)
   http.HandleFunc("/css/", serveResource)
   http.HandleFunc("/js/", serveResource)
-  http.HandleFunc("/", serveHtml)
+  http.HandleFunc("/", handlePath)
   http.HandleFunc("/webservice/login", loginService)
 
   fmt.Println("Intoxicating Inquiry server listening on port " + port)
   log.Fatal(http.ListenAndServe(":" + port, nil))
 }
 
-func serveHtml(w http.ResponseWriter, r *http.Request) {
-  http.ServeFile(w, r, "assets/html/" + r.URL.Path[1:] + ".html")
+func isValidSession() bool {
+  return false
+}
+
+func handlePath(w http.ResponseWriter, r *http.Request) {
+  requestedUrlPath := r.URL.Path[1:]
+
+  if(requestedUrlPath == "login") {
+    if(isValidSession()) {
+      http.Redirect(w, r, "/home", http.StatusFound)
+    } else {
+      serveHtml(w, r, "login")
+    }
+  } else {
+    if(isValidSession()) {
+      serveHtml(w, r, requestedUrlPath)
+    } else {
+      http.Redirect(w, r, "/login", http.StatusFound)
+    }
+  }
+
+}
+
+func serveHtml(w http.ResponseWriter, r *http.Request, path string) {
+  if(path == "login") {
+    http.ServeFile(w, r, "assets/html/login.html")
+  } else if (path == "home"){
+
+  }
 }
 
 func serveResource(w http.ResponseWriter, r *http.Request) {
@@ -61,4 +88,5 @@ func loginService(w http.ResponseWriter, r *http.Request) {
   } else {
 
   }
+
 }
