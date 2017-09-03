dc2236c19ec4eaa4e237c5ffb87d5282647dc852
Gabe Douda <gabedouda@gmail.com>
2017-09-02 20:11:23 -0600

    added database sessions and config

--- server.go
+++ server.go
@@ -8,10 +8,23 @@ import (
   "crypto/sha256"
   "encoding/hex"
   "os"
+  "database/sql"
+  "io/ioutil"
+  "encoding/json"
+  _ "github.com/go-sql-driver/mysql"
 )
 
+type Config struct {
+  Db_Username string  `json:"db_username"`
+  Db_Password string  `json:"db_password"`
+  Db_Schema string  `json:"db_schema"`
+}
+
+var db *sql.DB
+var config Config
+
 func main() {
-  port := "8080";
+  port := "8080"
 
   if len(os.Args) > 1 {
     port = os.Args[1]
@@ -27,21 +40,75 @@ func main() {
   log.Fatal(http.ListenAndServe(":" + port, nil))
 }
 
-func isValidSession() bool {
-  return false
+func getConfig() Config {
+  if(config == Config{}) {
+    raw, err := ioutil.ReadFile("./config/config.json")
+    if(err != nil) {
+      log.Fatal("Could not load config, exiting...")
+    }
+    json.Unmarshal(raw, &config)
+  }
+
+  return config
+}
+
+func getDatabaseConnection() *sql.DB {
+  if(db == nil) {
+    config := getConfig()
+
+    var err error
+    db, err = sql.Open("mysql", config.Db_Username + ":" + config.Db_Password + "@/" + config.Db_Schema + "?charset=utf8")
+    if(err != nil) {
+      log.Fatal("Could not connect to database, exiting...")
+    }
+  }
+
+  return db
+}
+
+func getUserIdFromDb(username string, passwordHash string) int {
+  user_id := -1
+  getDatabaseConnection().QueryRow("SELECT user_id FROM user WHERE username=? AND password=? LIMIT 1", username, passwordHash).Scan(&user_id)
+
+  return user_id
+}
+
+func isValidSession(cookie *http.Cookie) bool {
+  returnValue := false
+
+  if(cookie != nil) {
+    sessionId := cookie.Value
+    fmt.Println("SessionId: " + sessionId)
+    user_id := -1
+    getDatabaseConnection().QueryRow("SELECT user_id FROM session WHERE session_id=? LIMIT 1", sessionId).Scan(&user_id)
+    fmt.Println("UserId: ", user_id)
+    if(user_id != -1) {
+      returnValue = true
+    }
+  }
+
+  fmt.Println("ValidSession: ", returnValue)
+
+  return returnValue
 }
 
 func handlePath(w http.ResponseWriter, r *http.Request) {
   requestedUrlPath := r.URL.Path[1:]
+  fmt.Println(requestedUrlPath)
+
+  cookie, err := r.Cookie("intoxicating_inquiry_session_cookie")
+  if(err != nil) {
+    logError(err)
+  }
 
   if(requestedUrlPath == "login") {
-    if(isValidSession()) {
+    if(isValidSession(cookie)) {
       http.Redirect(w, r, "/home", http.StatusFound)
     } else {
       serveHtml(w, r, "login")
     }
   } else {
-    if(isValidSession()) {
+    if(isValidSession(cookie)) {
       serveHtml(w, r, requestedUrlPath)
     } else {
       http.Redirect(w, r, "/login", http.StatusFound)
@@ -54,7 +121,7 @@ func serveHtml(w http.ResponseWriter, r *http.Request, path string) {
   if(path == "login") {
     http.ServeFile(w, r, "assets/html/login.html")
   } else if (path == "home"){
-
+    http.ServeFile(w, r, "assets/html/asfd.html")
   }
 }
 
@@ -69,11 +136,11 @@ func loginService(w http.ResponseWriter, r *http.Request) {
   password := r.Form.Get("password")
   hasher := sha256.New()
   hasher.Write([]byte(password))
-  hashedPassword := hex.EncodeToString(hasher.Sum(nil))
+  passwordHash := hex.EncodeToString(hasher.Sum(nil))
 
   t := time.Now()
 
-  sessionIdString := username + ":" + hashedPassword + ":" + t.String()
+  sessionIdString := username + ":" + passwordHash + ":" + t.String()
   fmt.Println("SessIDString: " + sessionIdString)
 
   hasher = sha256.New()
@@ -81,12 +148,30 @@ func loginService(w http.ResponseWriter, r *http.Request) {
   sessionIdHash := hex.EncodeToString(hasher.Sum(nil))
   fmt.Println("SessIdHash: " + sessionIdHash)
 
-  if(true) {
-    cookie := http.Cookie{Name: "intoxicating_inquiry_session_cookie", Value: sessionIdHash, Expires: time.Now().Add(1 * 24 * time.Hour), Path: "/"}
-    http.SetCookie(w, &cookie)
-    http.Redirect(w, r, "/home", http.StatusFound)
+  userId := getUserIdFromDb(username, passwordHash)
+  if(userId != -1) {
+    stmt, err := db.Prepare("INSERT INTO session (`session_id`, `user_id`) VALUES (?, ?)")
+    fmt.Println("Inserting SessionId: " + sessionIdHash)
+    if(err != nil) {
+      logError(err)
+    } else {
+      _, err = stmt.Exec(sessionIdHash, userId)
+      if(err != nil) {
+        logError(err)
+      } else {
+        fmt.Println("Logged in user: ", userId)
+        cookie := http.Cookie{Name: "intoxicating_inquiry_session_cookie", Value: sessionIdHash, Expires: time.Now().Add(1 * 24 * time.Hour), Path: "/"}
+        http.SetCookie(w, &cookie)
+        fmt.Println("Sending redirect")
+        http.Redirect(w, r, "/home", http.StatusFound)
+      }
+    }
   } else {
-
+    // handle invalid login
   }
 
+}
+
+func logError(err error) {
+  log.Println("Error: ", err)
 }
